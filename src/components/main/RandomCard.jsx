07fbc4ab0ea526ed81cb94ead2ff22c2d56eaf1d
nahidaghazada <agazada.nahid@gmail.com>
2025-06-28 12:44:32 +0400

    add search

--- src/components/main/RandomCard.jsx
+++ src/components/main/RandomCard.jsx
@@ -1,10 +1,8 @@
 import { Link } from "react-router-dom"
 
+function RandomCard({ flags, name, cca3, capital, region, borders, area, population }) {
 
-function RandomCard({flags, name, cca3, capital, region, borders, area, population}) {
-  
     return (
-
         <Link to={`/Details/${name.common}`} className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-50">
             <img src={flags?.svg} alt={flags?.alt} className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
             <div className="p-6 space-y-2 lg:col-span-5">
@@ -13,6 +11,14 @@ function RandomCard({flags, name, cca3, capital, region, borders, area, populati
                 <p>Country: {cca3}</p>
                 <p>Area:{area}</p>
                 <p>Population: {population}</p>
+
+                <div className="flex flex-wrap gap-2 mt-2">
+                    {borders?.map((item, index) => (
+                        <Link key={index} to={`/details/${item}`} className="px-4 py-2 text-sm font-semibold rounded bg-indigo-500 text-white">
+                            {item}
+                        </Link>
+                    ))}
+                </div>
             </div>
         </Link>
     )
