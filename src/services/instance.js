cdd867b1d6d97216f49dc1a967192c00b426bfbb
nahidaghazada <agazada.nahid@gmail.com>
2025-07-22 14:53:41 +0400

    add and delete compinents

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://restcountries.com/v3.1"
+const BASE_URL = import.meta.env.VITE_BASE_URL
 
 export const instance = axios.create({
     baseURL: BASE_URL,
